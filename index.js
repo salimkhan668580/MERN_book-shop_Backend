53bdb35710e747e3aef4bcb3120203a4e81c6493
salimkhan668580 <salimkhan668580s@gmail.com>
2024-07-17 16:33:53 +0530

    this is first commit

---
+++ index.js
@@ -0,0 +1,135 @@
+
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const cors=require('cors');
+app.use(cors())
+app.use(express.json());
+const port = process.env.PORT || 3000
+const Razorpay = require('razorpay');
+const dbUsername = process.env.DB_USERNAME;
+const dbPassword = process.env.DB_PASSWORD;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const uri = `mongodb+srv://${dbUsername}:${dbPassword}@cluster0.6fip419.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    const bookCollections = client.db("BookInventory").collection("Books");
+
+
+    // insert a book to db: Post Method
+    app.post("/upload-book", async (req, res) => {
+        const data = req.body;
+        // console.log(data);
+        const result = await bookCollections.insertOne(data);
+        res.send(result);
+    })
+
+    // // get all books from db
+    // app.get("/all-books", async (req, res) => {
+    //     const books = bookCollections.find();
+    //     const result = await books.toArray();
+    //     res.send(result)
+    // })
+
+    // get all books & find by a category from db
+    app.get("/all-books", async (req, res) => {
+        let query = {};
+        if (req.query?.category) {
+            query = { category: req.query.category }
+        }
+        const result = await bookCollections.find(query).toArray();
+        res.send(result)
+    })
+
+    // update a books method
+    app.patch("/book/:id", async (req, res) => {
+        const id = req.params.id;
+       
+        const updateBookData = req.body;
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+
+        const updatedDoc = {
+            $set: {
+                ...updateBookData
+            }
+        }
+        
+        // update now
+        const result = await bookCollections.updateOne(filter, updatedDoc, options);
+        res.send(result);
+    })
+
+
+    // delete a item from db
+    app.delete("/book/:id", async (req, res) => {
+        const id = req.params.id;
+       
+    
+  
+        const result = await bookCollections.deleteOne({ _id: new ObjectId(id)});
+
+        res.send(result);
+    })
+
+
+    // get a single book data
+    app.get("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await bookCollections.findOne(filter);
+      res.send(result)
+  })
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_ID_KEY,
+  key_secret: process.env.RAZORPAY_SECRET_KEY,
+});
+app.get('/', (req, res) => {
+   
+  res.send('Hello World!')
+
+})
+
+app.get("/order",async(req, res) => {
+
+  const data=  await razorpay.orders.create({
+    "amount": (200*100),
+    "currency": "INR",
+    "receipt": "RCTP_ID"+Date.now(),
+    
+  })
+  res.json({
+    amount:data.amount,
+    orderID:data.id
+  })
+})  
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
